0614d4811b74e8ba639c6bf2bf233710e5f67a68
Pasquale Guglielmi <guglielmipasquale@yahoo.it>
2016-10-26 19:45:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Udacity Feed Reader Project
+--------------------
+### How to run the app
+To run the app just clone this repository and load the index.html file in your browser. At the bottom of the page you'll find all the tests that can be run by Jasmine ( a JavaScript testing framework). Tests are grouped into suites, each of which refers to a specific behavior that the app must satisfy.
+To launch a specific test you can just click on it. There is also a link to run all tests together. If a test will fail then Jasmine will notify it to you.
+
+### Project Overview
+
+In this project I was given a web-based application that reads RSS feeds. The original developer of this application clearly saw the value in testing, already including [Jasmine](http://jasmine.github.io/) and even starting writing their first test suite! Unfortunately, they decided to move on to start their own company and I'm left with an application with an incomplete test suite. That's where my job comes in.
+
+### Why this Project?
+
+Testing is an important part of the development process and many organizations practice a standard of development known as "test-driven development". This is when developers write tests first, before they ever start developing their application. All the tests initially fail and then they start writing application code to make these tests pass.
+
+Whether you work in an organization that uses test-driven development or in an organization that uses tests to make sure future feature development doesn't break existing features, it's an important skill to have!
+
+Writing effective tests requires analyzing multiple aspects of an application including the HTML, CSS and JavaScript - an extremely important skill when changing teams or joining a new company.
+
+
+### Resources:
+
+- Udacity JavaScript Testing [course](https://www.udacity.com/course/ud549)
+- Jasmine [documentation](http://jasmine.github.io)
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,127 @@
+$(function() {
+    describe('RSS Feeds', function() {
+        /* This is our first test - it tests to make sure that the
+         * allFeeds variable has been defined and that it is not
+         * empty.
+         */
+        it('are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+
+
+        /* Test that loops through each feed
+         * in the allFeeds object and ensures it has a URL defined
+         * and that the URL is not empty.
+         */
+         it('each feed has a defined and not empty URL', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe('');
+            });
+         })
+
+        /* Test that loops through each feed
+         * in the allFeeds object and ensures it has a name defined
+         * and that the name is not empty.
+         */
+         it('each feed has a defined and not empty name', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
+            });
+         })
+    });
+
+    describe('The menu', function() {
+        /* When the body is assigned the 'menu-hidden' class then
+         * the .slide-menu element is translated out of the window
+         * by a css 'transform: translate3d' so it is NOT visible.
+         * Executing the following method checks if menu is currently
+         * visible or not
+         */
+        function menuIsVisible() {
+            return !$('body').hasClass('menu-hidden');
+        }
+        /* Test that ensures the menu element is
+         * hidden by default
+         */
+         it('is hidden when page is loaded', function() {
+            expect(menuIsVisible()).toBeFalsy();
+         });
+        /* Test that ensures the menu changes
+         * visibility when the menu icon is clicked
+         */
+         it('his visibility is triggered when the menu icon is clicked', function() {
+            $('.menu-icon-link').click();
+            expect(menuIsVisible()).toBeTruthy();
+            /* Clicking again should trigger the menu visibility once more
+             */
+            $('.menu-icon-link').click();
+            expect(menuIsVisible()).toBeFalsy();
+         });
+    });
+
+    describe('Initial Entries', function() {
+        /* This method returns the number of <a> links inside the .feed <div>
+         */
+        function entryElements() {
+            return $('.feed > a').length;
+        }
+        /* Once loadFeed() has been executed the done(), passed to loadFeed()
+         * as the second argument, is called from loadFeed() itself
+         */
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it('when loadFeed function is completed, there is at least one .entry element inside the .feed <div>', function() {
+            expect(entryElements()).toBeGreaterThan(0);
+        });
+    });
+
+    /* TODO: Write a new test suite named "New Feed Selection"
+        /* TODO: Write a test that ensures when a new feed is loaded
+         * by the loadFeed function that the content actually changes.
+         * Remember, loadFeed() is asynchronous.
+         */
+    describe('New Feed Selection', function() {
+        /* Variables to compare contents before and after the
+         * new feed loading (I'm also checking that the header
+         * title is actually changing)
+         */
+        var currentFeedContent,
+            currentHeaderTitle,
+            newFeedContent,
+            newHeaderTitle;
+        /* The feedback function passed to the loadFeed function
+         * gets the page content after the first feed loading. After
+         * that, it calls loadFeed() once again passing it a new feed
+         * id and a new callback function that will get the new page
+         * content and call done()
+         */
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                currentFeedContent = $('.feed').html();
+                currentHeaderTitle = $('.header-title').html();
+                loadFeed(1, function() {
+                    newFeedContent = $('.feed').html();
+                    newHeaderTitle = $('.header-title').html();
+                    done();
+                });
+            });
+        });
+        /* After the test I need to reload the initial content
+         */
+        afterEach(function() {
+            loadFeed(0);
+        })
+
+        it('when a new feed is loaded the content changes', function() {
+            expect(currentFeedContent).not.toBe(newFeedContent);
+            expect(currentHeaderTitle).not.toBe(newHeaderTitle);
+        });
+    });
+}());
